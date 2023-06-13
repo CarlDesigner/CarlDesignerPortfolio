7c5f215a6e7baed5b28880fdf6a9772b65eece26
Carlos Fajardo <61030996+CarlDesigner@users.noreply.github.com>
2023-06-13 09:37:46 +0000

    *AJUSTES* 404, dark theme, title env, readme.md, url site

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = "CarlDesigner | Personal Portfolio";
+export const SITE_DESCRIPTION ="¡Bienvenido a mi portafolio! Soy un apasionado de la tecnología y me especializo en el desarrollo de soluciones creativas y eficientes. Con una sólida formación en Desarrollo de Software y una amplia experiencia en diferentes lenguajes de programación y tecnologías, me dedico a crear aplicaciones web de alta calidad. Saludos ✌";
