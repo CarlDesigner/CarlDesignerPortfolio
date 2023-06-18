16d6fc050fb25ed4981517f38bc1d80caba55155
Carlos Fajardo <61030996+CarlDesigner@users.noreply.github.com>
2023-06-18 08:37:39 +0000

    Integration theme changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,40 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes:[
+      		"light",
+      		"dark",
+      		"cupcake",
+      		"bumblebee",
+      		"emerald",
+      		"corporate",
+      		"synthwave",
+      		"retro",
+      		"cyberpunk",
+      		"valentine",
+      		"halloween",
+      		"garden",
+      		"forest",
+      		"aqua",
+      		"lofi",
+      		"pastel",
+      		"fantasy",
+      		"wireframe",
+      		"black",
+      		"luxury",
+      		"dracula",
+      		"cmyk",
+      		"autumn",
+      		"business",
+      		"acid",
+      		"lemonade",
+      		"night",
+      		"coffee",
+      		"winter",
+			  ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+		
 	  }
 }
