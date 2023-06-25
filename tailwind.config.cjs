be0057b857743a0f976b9e57e24abf2d9d41119b
Carlos Fajardo <61030996+CarlDesigner@users.noreply.github.com>
2023-06-25 07:28:42 +0000

    Favicon ok - Hotfix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
 	
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes:[ // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes:[
       		"light",
       		"dark",
       		"cupcake",
@@ -34,7 +34,7 @@ module.exports = {
       		"night",
       		"coffee",
       		"winter",
-			  ],
+			  ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		
 		darkTheme: "dark", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
