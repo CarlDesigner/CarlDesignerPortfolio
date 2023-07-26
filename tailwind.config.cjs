eda3e68ac22fbda4202e5e9aad308f76a00f20a7
Gregorovich <105803501+Gregorovich-Makarovich@users.noreply.github.com>
2023-07-26 15:30:27 +0000

    Estilo nuevo para tema Dark

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -36,7 +36,7 @@ module.exports = {
       		"winter",
 			  ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		
-		darkTheme: "dark", // name of one of the included themes for dark mode
+		darkTheme: "luxury", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 		
 	  }
