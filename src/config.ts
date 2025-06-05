5d557f507dd254ccb9d4b9c0213658dc5ca7e28e
CarlDesigner <krlitosfajardo626@gmail.com>
2025-06-05 15:08:18 -0500

    🔥 [REMOVE] New design

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "CarlDesigner | Personal Portfolio";
-export const SITE_DESCRIPTION ="¡Bienvenido a mi portafolio! Soy un apasionado de la tecnología y me especializo en el desarrollo de soluciones creativas y eficientes. Con una sólida formación en Desarrollo de Software y una amplia experiencia en diferentes lenguajes de programación y tecnologías, me dedico a crear aplicaciones web de alta calidad. Saludos ✌";
--- src/content/blog/post1.md
+++
@@ -1,6 +0,0 @@
----
-title: "Demo Post 1"
-description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-pubDate: "Sep 10 2022"
-heroImage: "https://res.cloudinary.com/do66lfayc/image/upload/v1686551213/Portfolio/imgBlog/Piloto_pbvoab.jpg"
----
