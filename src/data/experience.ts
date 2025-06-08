b86889e427461d15cfe3e189c52d0614d21cf159
CarlDesigner <krlitosfajardo626@gmail.com>
2025-06-07 21:21:03 -0500

    🚀 [DEPLOY] Projetc

---
+++ src/data/experience.ts
@@ -0,0 +1,62 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "Octubre 2024 — Actualidad",
+    title: "Desarrollador Web Freelance",
+    company: "Freelancer (Remoto)",
+    companyUrl: "https://wa.me/573202028049?text=Hola%2C%20me%20interesa%20tu%20trabajo",
+    description:
+      "He trabajado con más de 30 clientes a través de plataformas como Freelancer.com, Fiverr y de manera directa con emprendedores y pequeñas marcas, desarrollando tiendas en línea y sitios web modernos adaptados a sus necesidades. Actualmente, mantengo una base activa de clientes recurrentes, brindando soporte, mejoras continuas y nuevos desarrollos según sus objetivos de negocio.",
+    technologies: [
+      "WooCommerce",
+      "Shopify",
+      "Astro",
+      "Tailwind CSS",
+      "Firebase",
+      "Shopify",
+      "WordPress",
+      "Figma",
+      "Google Ads",
+      "Landing pages",
+      "Facebook & Instagram Ads",
+      "Google My Business",
+    ],
+    logo: "/logos/free.webp",
+  },
+  {
+    dates: "Septiembre 2022 — Noviembre 2024",
+    title: "Especialista en Ecommerce y UX/UI",
+    company: "Shark Agencia",
+    companyUrl: "https://wa.me/573202028049?text=Hola%2C%20me%20interesa%20tu%20trabajo",
+    description:
+      "En Shark Agencia trabajé como desarrollador especializado en tiendas online, creando ecommerce funcionales y enfocados en la conversión. Apliqué principios de UX/UI, integré pasarelas de pago como PayPal, Stripe, Wompi y PayU, y utilicé Google Analytics para analizar y optimizar el rendimiento. Contribuí al desarrollo de múltiples tiendas exitosas, ayudando a marcas a mejorar su presencia digital y aumentar sus ventas.",
+    technologies: [
+      "React JS",
+      "UX/UI",
+      "Google Analytics",
+      "Node JS",
+      "Shopify Storefront API",
+      "BigCommerce",
+      "Magento",
+    ],
+    logo: "/logos/shark.webp",
+  },
+  {
+    dates: "Abril 2019 — Agosto 2022",
+    title: "Soporte en Seguridad Electrónica",
+    company: "Securitas Colombia",
+    companyUrl: "https://wa.me/573202028049?text=Hola%2C%20me%20interesa%20tu%20trabajo",
+    description:
+      "Brindé soporte técnico especializado en sistemas de seguridad electrónica como cámaras IP, DVRs, control de acceso y alarmas, integrando herramientas digitales y programación para automatizar procesos y mejorar la eficiencia del servicio, Desarrollé paneles internos para monitoreo y gestión de incidencias.",
+    technologies: [
+      "Sistemas de Seguridad",
+      "Soporte Técnico",
+      "Controles de acceso biométricos",
+      "Mantenimiento preventivo",
+      "Mantenimiento correctivo",
+      "Monitoreo remoto",
+    ],
+    logo: "/logos/securit.webp",
+  },
+];
